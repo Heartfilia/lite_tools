390cf13dbf98bcfa658f7deaa7f2864e2c416914
Heartfilia <lodge6410@vip.qq.com>
2021-05-27 18:36:44 +0800

    迭代了try_get_by_name和修改了ua库增加chrome选择

--- build/lib/lite_tools/dict_parser.py
+++ build/lib/lite_tools/dict_parser.py
@@ -3,6 +3,7 @@
 # @Author : Lodge
 import re
 import random
+from sys import flags
 from loguru import logger
 
 
@@ -41,7 +42,7 @@ def try_get(renderer: dict, getters, default=None, expected_type=None, log=False
         return default
 
 
-def try_get_by_name(renderer: dict, getter: str, depth: int = 50, in_list: bool = True, expected_type=None, log: bool = False) -> list:
+def try_get_by_name(renderer: dict, getter: str, depth: int = 50, in_list: bool = True, expected_type=None, filter: list = None, log: bool = False) -> list:
     """
     通过名称获取字典里面的字符  这里做底层就是避免有的人乱调用
     :param renderer : 传入的字典
@@ -50,9 +51,11 @@ def try_get_by_name(renderer: dict, getter: str, depth: int = 50, in_list: bool 
     :param expected_type : 期望获得的值类型 不是则为None  可多传如：  expected_type=(list, str)
     :param log: 是否打印报错的提示日志 默认不打印
     :param in_list: 如果结果在一个列表里面的`字典`里面是否获取 默认获取 只判断列表里面的`字典`的值
+    :param filter: 过滤器 -- 目前这个版本更新的是兄弟关系获取 如: [key=value]  key=value 键值关系 目前取值范围只有一个关系 兄弟关系 只会处理同一级字典内容的判断
+                                                            示例: ["status>=200", "name='lodge'"]  表示获取的getter里面同一级关系为status>=200 和 name='lodge' 
     """
     try:
-        result = __try_get_by_name(renderer=renderer, getter=getter, depth=depth, expected_type=expected_type, in_list=in_list)
+        _ = __try_get_by_name(renderer=renderer, getter=getter, depth=depth, expected_type=expected_type, filter=filter, in_list=in_list)
     except (AttributeError, KeyError, TypeError, IndexError) as e:
         if log is True:
             logger.error(f"try_get_by_name: {e} -- {type(e)} --line: {e.__traceback__.tb_lineno}")
@@ -61,7 +64,7 @@ def try_get_by_name(renderer: dict, getter: str, depth: int = 50, in_list: bool 
         return res.args[0] if res.args else []
 
 
-def __try_get_by_name(renderer: dict, getter: str, expected_type, in_list, result: list = [], depth: int = 50, is_first=True) -> list:
+def __try_get_by_name(renderer: dict, getter: str, expected_type, in_list, result: list = None, filter=None, depth: int = 50, is_first=True) -> list:
     """
     通过名称获取字典里面的字符  这里做底层就是避免有的人乱调用
     :param renderer : 传入的字典
@@ -70,35 +73,40 @@ def __try_get_by_name(renderer: dict, getter: str, expected_type, in_list, resul
     :param depth : 遍历深度,默认50层
     :param is_first : 是否是第一次传入,外面不需要传这个数据
     :param in_list: 如果结果在一个列表里面的`字典`里面是否获取 默认不获取 只判断列表里面的`字典`的值
+    :param filter: 过滤器--> 只处理同级关系
     """
     if is_first is True:
         result = []
     if depth < 0 or not renderer:
         raise Exception(result)
     need_parse_next_renderer = dict()
-
     for key, value in renderer.items():
         if re.sub(r"#_#\d+", "", key) == getter:
             if expected_type is not None and not isinstance(value, expected_type):
                 if isinstance(value, dict):
                     need_parse_next_renderer.update(__do_dict_sample(value))
                 continue
-            result.append(value)
+            if filter is not None:
+                ok = __do_filter_func(filter, renderer)
+                if ok:
+                    result.append(value)
+            else:
+                result.append(value)
 
         if isinstance(value, dict):
             need_parse_next_renderer.update(__do_dict_sample(value))
         if isinstance(value, list) and in_list is True:
             for item in value:
                 if isinstance(item, dict):
-                    res, need_dict = __handle_to_dict(item, getter, expected_type)
+                    res, need_dict = __handle_to_dict(item, getter, filter, expected_type)
                     result += res
                     need_parse_next_renderer.update(need_dict)
 
     depth -= 1
-    return __try_get_by_name(need_parse_next_renderer, getter, expected_type, in_list, result, depth=depth, is_first=False)
+    return __try_get_by_name(need_parse_next_renderer, getter, expected_type, in_list, result, filter=filter, depth=depth, is_first=False)
 
 
-def __handle_to_dict(data: dict, getter, expected_type):
+def __handle_to_dict(data: dict, getter, filter, expected_type):
     back_dict = dict()
     back_result = list()
     for key, value in data.items():
@@ -107,10 +115,19 @@ def __handle_to_dict(data: dict, getter, expected_type):
                 if isinstance(value, dict):
                     back_dict.update(__do_dict_sample(value))
                 continue
-            back_result.append(value)
+            if filter is not None:
+                ok = __do_filter_func(filter, data)
+                if ok:
+                    back_result.append(value)
+            else:
+                back_result.append(value)
 
         if isinstance(value, dict):
             back_dict.update(__do_dict_sample(value))
+        
+        if isinstance(value, list):
+            back_dict.update({f"need_parse_next#_#{random.randint(1, 9999999999)}": value})
+        
     return back_result, back_dict 
 
 
@@ -120,3 +137,60 @@ def __do_dict_sample(data: dict):
         new_key = f"{new_key}#_#{random.randint(1, 9999999999)}"
         back_dict[new_key] = new_value
     return back_dict
+
+
+def __do_filter_func(filter, renderer):
+    first = True    # 判断是否第一次给默认值
+    ok = False
+    for rule in filter:
+        if first:
+            ok = True
+            first = False
+        flag = __handle_calculation(rule, renderer)
+        if flag is True:
+            ok = ok and True
+        else:
+            ok = ok and False
+    return ok
+
+
+def __handle_calculation(rule, renderer) -> bool:
+    if '==' in rule:
+        rule.replace('==', '=')
+    if "=" in rule:  
+        filter_rule = rule.split('=')
+        filter_key = str(filter_rule[0])
+        filter_value = filter_rule[1]
+        if renderer[filter_key] == eval(filter_value):
+            return True
+    elif ">" in rule:
+        filter_rule = rule.split('>')
+        filter_key = str(filter_rule[0])
+        filter_value = filter_rule[1]
+        if renderer[filter_key] > eval(filter_value):
+            return True
+    elif ">=" in rule:
+        filter_rule = rule.split('>=')
+        filter_key = str(filter_rule[0])
+        filter_value = filter_rule[1]
+        if renderer[filter_key] >= eval(filter_value):
+            return True
+    elif "<" in rule:
+        filter_rule = rule.split('<')
+        filter_key = str(filter_rule[0])
+        filter_value = filter_rule[1]
+        if renderer[filter_key] < eval(filter_value):
+            return True
+    elif "<=" in rule:
+        filter_rule = rule.split('<=')
+        filter_key = str(filter_rule[0])
+        filter_value = filter_rule[1]
+        if renderer[filter_key] <= eval(filter_value):
+            return True
+    elif "!=" in rule:
+        filter_rule = rule.split('!=')
+        filter_key = str(filter_rule[0])
+        filter_value = filter_rule[1]
+        if renderer[filter_key] != eval(filter_value):
+            return True
+    return False
--- lite_tools/ua_info.py
+++ lite_tools/ua_info.py
@@ -6,6 +6,7 @@ from user_agent import generate_user_agent, generate_navigator, generate_navigat
 
 
 def get_ua(*args, **kwargs):
+    """下面的都是实际存在的浏览器版本 很多没有拓展 不过chrome的已经够用了"""
     obj_list = []
     if 'pc' in args or 'PC' in args:
         obj_list = ua_pc
@@ -21,8 +22,12 @@ def get_ua(*args, **kwargs):
                 obj_list += ua_linux
             if plt.lower() == 'ios':
                 obj_list += ua_ios
-            if plt.lower() == 'win':
+            if plt.lower() == 'win':   # 这里还缺少firefox版本 后续增加现在这个和chrome相等
                 obj_list += ua_win
+            if plt.lower() == 'chrome':
+                obj_list += ua_chrome
+            if plt.lower() == 'ie':
+                obj_list += ua_ie
         if not obj_list:
             obj_list = ua_win
     random_ua = random.choice(obj_list)
@@ -49,9 +54,6 @@ ua_android = [
     'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Mobile Safari/537.36',
     'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Mobile Safari/537.36 Edg/90.0.818.46',
     'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Mobile Safari/537.36 Edg/90.0.818.46',
-    'Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
-    'Opera/9.80 (Android 2.3.4; Linux; Opera Mobi/build-1107180945; U; en-GB) Presto/2.8.149 Version/11.10',
-    'MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
     'Mozilla/5.0 (hp-tablet; Linux; hpwOS/3.0.0; U; en-US) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/233.70 Safari/534.6 TouchPad/1.0'
 ]
 
@@ -59,18 +61,36 @@ ua_ios = [
     'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
     'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
     'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1',
-    'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
+    'Mozilla/5.0 (iPhone; U; CPU iPhone OS 14_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
 ]
 
-ua_win = [
+ua_chrome = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.16 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.20 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.69 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.14 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.39 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.30 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.38 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.22 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.20 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.27 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.23 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.24 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
-    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
-    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;',
-    'Mozilla/5.0 (Windows NT 6.1; rv,2.0.1) Gecko/20100101 Firefox/4.0.1',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
-]
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.19 Safari/537.36',
+]  # version 80+
+
+ua_ie = [
+    'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.3; Trident/7.0; .NET4.0E; .NET4.0C)'
+]  # version 11
 
 ua_linux = [
     'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/51.0',
@@ -78,16 +98,16 @@ ua_linux = [
     'Mozilla/5.0 (X11; Linux i686 on x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.2792.97 Safari/537.36',
     'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0',
     'Mozilla/5.0 (X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/60.0',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.3578.98 Safari/537.36',
 ]
 
 ua_macos = [
-    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv,2.0.1) Gecko/20100101 Firefox/4.0.1',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser)',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_9_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv,2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/80.0.963.56 Safari/535.11',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.3626.121 Safari/537.36'
 ]
 
+ua_win = ua_chrome
 ua_pc = ua_win + ua_macos + ua_linux
 ua_mobile = ua_ios + ua_android
