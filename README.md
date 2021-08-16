dc75e1f6776438bfb6ead8ed61cd98dfcb5535f8
Heartfilia <lodge6410@vip.qq.com>
2021-08-16 19:15:23 +0800

    修改了异常捕获的提取问题--try_catch

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ pip install lite-tools
 ```python
 get_time()         # 时间操作 示例见demo.py
 get_ua()           # 获取随机ua 
-# get_navigator()    # 已经移除这个特性
+timec              # 时间统计装饰器
 
 # ==============下面是推荐使用的功能===================
 try_get()          # 获取*字典或者json串*里面的键的值 返回一个结果 没有默认返回None // 没有获取到期望类型也是None
@@ -37,6 +37,15 @@ get_sha()          # 用sha加密 默认mode=256 可以选择其它加密模式
 get_sha3()         # 用sha3加密 默认mode=256
 get_b64e()         # 用baseXX加密 默认mode=64 可以输出字节串
 get_b64d()         # 用baseXX解密 默认mode=64
+
+# ==================================
+# 新增两个异常捕获装饰器
+try_catch
+try_catch(log=False)
+
+async_try_catch     
+# 这个暂时不对外使用 还需要调整一下 如果要测试 可以用 
+from lite_tools.try_decorater import async_try_catch
 
 # Example in 'demo.py'  ||  示例见demo.py
 ```
---
+++ demo/try_catch_demo.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from lite_tools import try_catch
+
+
+# try_catch 适用于单一的功能判断的异常捕获 不建议被装饰的函数里面再写循环之类的:成功返回结果 失败返回None
+# try_catch 默认是打印日志的
+# try_catch(log=False)  则不打印日志
+
+# async_try_catch  这个没有参数可以选择,异常捕获不确定是否会有什么问题，还需要发现
+
+
+@try_catch
+def start_run(n):
+    # 这里面正常处理即可 如果报错异常直接返回的None 所以不建议任何地方都用这个处理 如果明确返回值也有可能是None的就别这里处理了
+    if n < 5:
+        raise TimeoutError
+    return n
+
+
+def circle_test():
+    for ind in range(10):
+        result = start_run(ind)
+        if result: return result
+
+# 2021-07-19 17:27:12.358 | ERROR    | lite_tools.try_decorater:wrapper:25 - [start_run: TimeoutError] detail: 
+# 2021-07-19 17:27:12.361 | ERROR    | lite_tools.try_decorater:wrapper:25 - [start_run: TimeoutError] detail: 
+# 2021-07-19 17:27:12.368 | ERROR    | lite_tools.try_decorater:wrapper:25 - [start_run: TimeoutError] detail: 
+# 2021-07-19 17:27:12.373 | ERROR    | lite_tools.try_decorater:wrapper:25 - [start_run: TimeoutError] detail: 
+# 2021-07-19 17:27:12.376 | ERROR    | lite_tools.try_decorater:wrapper:25 - [start_run: TimeoutError] detail: 
+# 5
+
+if __name__ == "__main__":
+    print(circle_test())
