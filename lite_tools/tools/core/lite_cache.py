085d3c74116742737b24701500bd517b9f72acb1
Heartfilia <lodge6410@vip.qq.com>
2022-08-25 16:15:43 +0800

    cache加了但是不保证稳定

--- lite_tools/tools/core/lite_cache.py
+++ lite_tools/tools/core/lite_cache.py
@@ -18,11 +18,16 @@
           ┃ ┫ ┫   ┃ ┫ ┫
           ┗━┻━┛   ┗━┻━┛
 这里是搞一个缓存中间件 通过装饰器的方案 让多线程或者多进程里面的消费者或者生产者走同一个管道
+
+>> 第一版先只兼容同步的函数   **异步没有兼容** <<
 """
 import time
-from threading import Lock, Thread, current_thread
+import asyncio
+from queue import Empty
+from typing import Union
+from functools import wraps
 from multiprocessing import Queue
-from functools import wraps, partial
+from threading import Lock, current_thread
 
 from lite_tools.tools.utils.logs import logger
 from lite_tools.exceptions.CacheExceptions import QueueEmptyNotion
@@ -56,96 +61,65 @@ class Singleton(type):
 
 class Buffer(metaclass=Singleton):
     queue_long: int = 10000   # 设置队列的长度 默认 10000   如果要改 --> Buffer.queue_long = 100
-    __task_flag = {}          # 队列什么时候结束由这里和队列长度一起说了算
-    __queues = {}             # 队列需要放个名字上 用字典取值 因为我要保证同一个buffer
+    __task_flag = True        # 队列什么时候结束由这里和队列长度一起说了算
+    __queues: Union[Queue, asyncio.Queue] = None           # 队列需要放个名字上 用字典取值 因为我要保证同一个buffer
     __task_count = set()      # 线程情况统计
     _lock: Lock = Lock()
 
     @classmethod
-    def seed(cls, name: str = "default"):
-        if cls.__queues.get(name) and not cls.__queues.get(name).empty():
-            try:
-                return cls.__queues.get(name).get()
-            except QueueEmptyNotion:
+    def seed(cls):
+        with cls._lock:
+            if not cls.__queues.empty():
+                try:
+                    return cls.__queues.get(timeout=3)
+                except Empty:
+                    raise QueueEmptyNotion
+            else:
                 raise QueueEmptyNotion
-        else:
-            raise QueueEmptyNotion
+
 
     @classmethod
-    def task(cls, func, name: str = "default"):
+    def task(cls, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             # 这里是一个独立的线程运行
-            cls.__task_flag[name] = True
+            cls.__task_flag = True
             for job in func(*args, **kwargs):
-                cls.__queues[name].put(job)
-            cls.__task_flag[name] = False
+                cls.__queues.put(job)
+            cls.__task_flag = False
 
-        if func is None:
-            return partial(cls.task, name=name)
-
-        if name not in cls.__queues:
-            cls.__queues[name] = Queue(cls.queue_long)
-
+        cls.__init__queue__()
         return wrapper
 
     @classmethod
-    def worker(cls, func, name: str = "default"):
+    def worker(cls, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             with cls._lock:
                 if current_thread().name not in cls.__task_count:
                     cls.__task_count.add(current_thread().name)
             while True:
-                logger.debug(f"{current_thread().name} // {cls.__task_flag.get(name)} // {cls.__queues[name].empty()}")
-                if not cls.__task_flag.get(name) and cls.__queues[name].empty():
+                if not cls.__task_flag and cls.__queues.empty():
                     with cls._lock:
                         if current_thread().name in cls.__task_count:
                             cls.__task_count.remove(current_thread().name)
                         if not cls.__task_count:
-                            logger.debug(f"[{name}] 队列-> 任务种子消耗完毕,worker结束.")
-                    logger.warning(
-                        f"{current_thread().name} // {cls.__task_flag.get(name)} // {cls.__queues[name].empty()}")
+                            logger.debug(f"队列-> 任务种子消耗完毕,worker结束.")
                     break
-                logger.info(f"{current_thread().name} // {cls.__task_flag.get(name)} // {cls.__queues[name].empty()}")
-                if cls.__queues[name].empty():
+                if cls.__queues.empty():
                     time.sleep(1)
                     continue
 
-                logger.success(f"{current_thread().name} // {cls.__task_flag.get(name)} // {cls.__queues[name].empty()}")
                 try:
                     func(*args, **kwargs)
                 except QueueEmptyNotion:
-                    logger.error("这里错了吗")
                     pass
 
-        if func is None:
-            return partial(cls.task, name=name)
-        if name not in cls.__queues:
-            cls.__queues[name] = Queue(cls.queue_long)
-
+        cls.__init__queue__()
         return wrapper
 
-
-@Buffer.task
-def test1():
-    for t in range(12):
-        yield t
-
-
-@Buffer.worker
-def test2():
-    task = Buffer.seed()
-    logger.info(task)
-
-
-if __name__ == "__main__":
-    Thread(target=test1).start()
-    t_list = [Thread(target=test2) for _ in range(5)]
-    for t in t_list:
-        # t.setDaemon(True)
-        t.start()
-    # for t in t_list:
-    #     t.join()
-    print('xxxxxxxxxx')
-
+    @classmethod
+    def __init__queue__(cls):
+        with cls._lock:
+            if not cls.__queues:
+                cls.__queues = Queue(cls.queue_long)
