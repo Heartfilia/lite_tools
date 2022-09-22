5da0dcd3e988e29ac491e6a9e821af2865ad7759
Heartfilia <lodge6410@vip.qq.com>
2022-09-22 18:54:14 +0800

    备份请求

--- lite_tools/tools/http/req_utils.py
+++ lite_tools/tools/http/req_utils.py
@@ -18,29 +18,64 @@
           ┃ ┫ ┫   ┃ ┫ ┫
           ┗━┻━┛   ┗━┻━┛
 """
+from typing import Literal, Union, TypeVar, Mapping
+
 import httpx
 import requests
 
-from typing import Literal, Union, TypeVar, Mapping
+from lite_tools.tools.time.lite_time import get_time
+
 
 T = TypeVar('T')
 
 
+class LiteHistory:
+    status_code: int = 200
+    url: str
+    cookie: dict
+    headers: dict
+
+    def __str__(self):
+        return f"<Response [{self.status_code}]>"
+
+    def __setattr__(self, key, value):
+        self.__dict__[key] = value
+
+    def __getattr__(self, item):
+        return self.__dict__[item]
+
+
 class LiteResponse:
     status_code: int = 200
-    response: ...
-    decode: str = 'requests'
-    headers: requests.Response.headers
-    history: requests.Response.history
+    url: str
+    request_time: float      # ms 单位
+    body: ...
+    decode: str   # 这里还不确定怎么弄
+    request_mode: str = 'requests'
+    cookies: Union[requests.Response.cookies, httpx.Response.cookies]
+    headers: Mapping[str, T]
+    history: LiteHistory
+
+    def __new__(cls):
+        cls.request_time = get_time(instance=float)
+
+    def __str__(self):
+        return f"<Response [{self.status_code}]>"
+
+    def __setattr__(self, key, value):
+        self.__dict__[key] = value
+
+    def __getattr__(self, item):
+        return self.__dict__[item]
 
 
 class LiteRequest(object):
     def __init__(self, retry: int = 5):
         self.retry = retry
 
-    def send(self,
-             url,
-             method: Literal['get', 'post'] = 'get',  # request, head, patch, put, delete, options这些不搞
+    def send(self, url,
+             *,
+             method: Literal['get', 'post', 'head', 'put', 'delete'] = 'get',  # request, patch, options这些不搞
              h2: bool = False,
              encoding: str = None,
              proxies: Union[dict, str] = None,
@@ -69,8 +104,8 @@ class LiteRequest(object):
         """
         pass
 
-    async def async_send(self,
-                         url,
+    async def async_send(self, url,
+                         *,
                          method: Literal['get', 'post'] = 'get',
                          h2: bool = False,
                          encoding: str = None,
