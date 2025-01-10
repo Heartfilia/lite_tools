fa255a4be386e1b45b6be920d6e3f40b7abdd612
Heartfilia <lodge6410@vip.qq.com>
2025-01-10 14:45:17 +0800

    pip

--- lite_tools/utils/pip_.py
+++ lite_tools/utils/pip_.py
@@ -2,15 +2,23 @@ import sys
 import subprocess
 
 
-def install(package_name: str, update: bool = False, source: str = ""):
+def install(package_name: str, update: bool = False, source: str = "", extra_source: str = "", trust_host: str = ""):
     """
     替换 pip.main([])
+    update        >>> --upgrade
+    source        >>> --index-url xxxx
+    extra_source  >>> --extra-index-url xxxx
+    trust_host    >>> --trusted-host xxx
     """
     exec_args = [sys.executable, '-m', 'pip', 'install', package_name]
     if update:
         exec_args.insert(4, '--upgrade')
 
     if source:
-        exec_args.extend(["-i", source])
+        exec_args.extend(["--index-url", source])
+    if extra_source:
+        exec_args.extend(["--extra-index-url", extra_source])
+    if trust_host:
+        exec_args.extend(["--trusted-host", trust_host])
 
     subprocess.run(exec_args)
