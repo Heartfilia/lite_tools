94fb0007faaadb2981ef54dea42cc3f12332a384
Heartfilia <lodge6410@vip.qq.com>
2025-01-10 14:57:31 +0800

    pip

--- lite_tools/utils/pip_.py
+++ lite_tools/utils/pip_.py
@@ -2,17 +2,27 @@ import sys
 import subprocess
 
 
-def install(package_name: str, update: bool = False, source: str = "", extra_source: str = "", trust_host: str = ""):
+def install(
+        package_name: str, update: bool = False, user: bool = False, quiet: bool = False,
+        source: str = "", extra_source: str = "", trust_host: str = "", target: str = "", proxy: str = ""):
     """
     替换 pip.main([])
     update        >>> --upgrade
+    user          >>> --user
+    quiet         >>> --quiet
     source        >>> --index-url xxxx
     extra_source  >>> --extra-index-url xxxx
     trust_host    >>> --trusted-host xxx
+    target        >>> --target xxx
+    proxy         >>> --proxy xxx
     """
     exec_args = [sys.executable, '-m', 'pip', 'install', package_name]
     if update:
         exec_args.insert(4, '--upgrade')
+    if user:
+        exec_args.append("--user")
+    if quiet:
+        exec_args.append("--quiet")
 
     if source:
         exec_args.extend(["--index-url", source])
@@ -20,5 +30,9 @@ def install(package_name: str, update: bool = False, source: str = "", extra_sou
         exec_args.extend(["--extra-index-url", extra_source])
     if trust_host:
         exec_args.extend(["--trusted-host", trust_host])
+    if target:
+        exec_args.extend(["--target", target])
+    if proxy:
+        exec_args.extend(["--proxy", proxy])
 
     subprocess.run(exec_args)
