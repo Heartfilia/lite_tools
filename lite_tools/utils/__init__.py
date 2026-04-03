0a6edc93775a95d14358415737198c1534f8819f
empty.zhu <empty.zhu@datastory.com.cn>
2026-04-03 10:11:42 +0800

    新增版本管理号管理问题

--- lite_tools/utils/__init__.py
+++ lite_tools/utils/__init__.py
@@ -19,5 +19,4 @@
           ┗━┻━┛   ┗━┻━┛
 """
 
-# 大版本.小版本.新增功能.bug修复 [-beta1 内部测试版本]
-VERSION = '0.4.14.4'   # 不出意外会在1.0.0.0版本出一个轻量级的爬虫框架 (后续迭代里面的模块基本都是拔插式的)
+from lite_tools.version import VERSION, __version__
---
+++ lite_tools/version.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# 大版本.小版本.新增功能.bug修复 [-beta1 内部测试版本]
+VERSION = "0.4.14.4"
+__version__ = VERSION
